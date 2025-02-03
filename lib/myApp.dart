77e1e828028342f85ccf40433ea6507f130e3db7
Iliyas Robin <iliyasahmedr@gmail.com>
2025-02-03 19:37:16 +0600

    updating
    updating

---
+++ lib/myApp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
