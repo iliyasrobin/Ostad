8d02e903437d0a9cea9ed7af49c420178acec00c
Iliyas Robin <iliyasahmedr@gmail.com>
2025-02-04 10:19:43 +0600

    updating

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -8,6 +8,7 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
