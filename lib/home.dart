b9d921aeb87815a29e1e5f56226d8b3e2546f998
Iliyas Robin <iliyasahmedr@gmail.com>
2025-02-05 23:11:47 +0600

    live-test-2

--- lib/home.dart
+++ lib/home.dart
@@ -12,8 +12,41 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ABC"),
+        title: Text("Employee"),
         centerTitle: true,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text("Add Employee"),
+            TextFormField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: "Name",
+
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: "Age",
+
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: "Salary",
+
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){}, child: Text("Add Employee")),
+          ],
+        ),
       ),
     );
   }
